bd08a2704bf8a125583081620e02666cb60742ec
Student <justinlo69125@gmail.com>
2025-07-16 12:10:25 -0700

    blah blah blah part 2

--- detect-people.py
+++ detect-people.py
@@ -1,38 +1,12 @@
-import jetson.inference
-import jetson.utils
-import cv2
+from nanoowl.owl_predictor import OwlPredictor
 
-net = jetson.inference.detectNet("pednet", threshold=0.5)
-cap = cv2.VideoCapture(0)
+predictor = OwlPredictor(
+    "google/owlvit-base-patch32",
+    image_encoder_engine="data/owlvit-base-patch32-image-encoder.engine"
+)
 
-if not cap.isOpened():
-    print("Webcam not accessible")
-    exit()
+image = PIL.Image.open("assets/owl_glove_small.jpg")
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+output = predictor.predict(image=image, text=["an owl", "a glove"], threshold=0.1)
 
-    img_rgba = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    cuda_img = jetson.utils.cudaFromNumpy(img_rgba)
-
-    detections = net.Detect(cuda_img)
-
-    for detection in detections:
-        if net.GetClassDesc(detection.ClassID) == "person":
-            left = int(detection.Left)
-            top = int(detection.Top)
-            right = int(detection.Right)
-            bottom = int(detection.Bottom)
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-            cv2.putText(frame, "Person", (left, top - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-    cv2.imshow("People Detection", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+print(output)
